24017c1662c5b7d1ab9bd9173e9b5d8d9056aef9
shubhamg <shubhamg200204@gmail.com>
2024-08-27 21:15:26 +0530

    a

---
+++ 13_bitwise_operators.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+// bitwise operators are used to play with bits (perform operations on data at bit level)
+// bitwise operators cannot be applied on 1.float 2.double 3.long double 4.bool 5.void and other more complex
+
+int main(){
+    return 0;
+}
