c17eae6a2e79daa7e1a38dde1a19ae6e45109d07
shubhamg <shubhamg200204@gmail.com>
2024-08-27 19:53:21 +0530

    a

--- 09_assignment_operator.c
+++ 09_assignment_operator.c
@@ -18,8 +18,33 @@ int main(){
     // left part should be always variable or pointer not function or constant
     
 
-    // type change 
-    // in assgnment type of left of = is converted to the type of right
-    a = 'c'; // int -> char  (here we represent 8 less significant bits to represent char)
-    printf("%c", a);
+
+    // ***** type conversion ***** 
+    // in assgnment value of left = is converted to the type of right
+
+    // int -> char  (here we represent 8 less significant bits to represent char)
+    char c;
+    c = 65; 
+    printf("%c\n", c);
+
+    // float -> int
+    float f = 1021.123;
+    int i = f; // here integer part of float is stored in int type var
+    printf("%d\n", i);
+
+    // float -> char
+    char g = 65.124532; // 8 least significant bits are stored in char
+    printf("%c\n", g);
+
+    // char -> float 
+    float f1 = 'A'; // converts char into floating point
+    printf("%f\n", f1);
+    // while converting from higer sized data types to lower (it reduces appropriate higher order bits);
+
+
+
+    // ***** multiple assignment ***** 
+    int x, y, z;
+    x = y = z = 10; // assigning single number to 3 vars;
+    printf("%d %d %d", x,y,z);
 }
