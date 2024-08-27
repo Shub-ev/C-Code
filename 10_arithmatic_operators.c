ad505b8d88d27aaf29ee15836bd5039ca4314ca6
shubhamg <shubhamg200204@gmail.com>
2024-08-27 20:03:34 +0530

    a

---
+++ 10_arithmatic_operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+    // ***** assignment operators *****
+    // used to perform arithmatic operators over vars
+    // + - / * % ++ --
+
+
+    // ++x != x++
+    // ++x -> first increment number then expression if
+    // x++ -> first expression 
+
+    // increment and decrement gererates more optimized/fast object file hence use --/++ 
+
+    // ***** operator precedence *****
+
+    // highest  ->          ()
+    //                    ++ , -- 
+    //                 -(unary minus)
+    //                 * , / , %
+    // lowest              +, -
+
+    // on same level left to right
+}
