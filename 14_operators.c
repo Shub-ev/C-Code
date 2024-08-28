11760249f516b2a54ae7a60f625191a1faf87d8a
shubhamg <shubhamg200204@gmail.com>
2024-08-28 09:39:19 +0530

    a

--- 14_operators.c
+++ 14_operators.c
@@ -1,7 +1,14 @@
 // ******  Other Operators  ******
 // 1. Ternary Operator
 
+// syntax =>    Exp1 ? Exp2 : Exp3
+
+// if Exp1 evaluates to true then value of Exp2 after evaluation will be result of statement
+// else if Exp1 evaluates to false then value of Exp3 after evaluation will be result of statement
+
 #include<stdio.h>
 int main(){
-    return 0;
+
+    printf("%s", (10 < 5 ? "True" : "False"));
+
 }
