5b7b72097cf82a71641e8c43a70451412092abb3
Shub-ev <shubhamg200204@gmail.com>
2025-03-15 23:48:55 +0530

    a

---
+++ 18_calloc.c
@@ -0,0 +1,29 @@
+// calloc() is also used to allocate dynamic memory
+// void* calloc( size_t num, size_t size );     // 2 parameters
+
+// ***** main point about calloc is it initializes each block to 0
+
+// returns void pointer, hence we need to cast to perticular object
+// calloc() has more overhead than melloc()
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int *ptr = (int*) calloc(5, sizeof(int));
+
+    if(ptr != NULL){
+        for(int i = 0; i < 5; i++){
+            *(ptr+i) = i * i;
+        }
+        for(int i = 0; i < 5; i++){
+            printf("%d\n", *(ptr + i));
+        }
+    }
+    else{
+        printf("Error in memory allocation!");
+    }
+
+    return 0;
+}
---
+++ 19_realloc.c
@@ -0,0 +1,35 @@
+// void *realloc( void *ptr, size_t new_size );
+// realloc() is used to dynamically increase or decrease the size of dynamically allocated 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr = (int*) calloc(5, sizeof(int));
+
+    if(ptr == NULL){
+        printf("Memory allocation Failed!");
+        return 1;
+    }
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", *(ptr + i));      // prints 5 0's
+    }
+    printf("\n");
+
+    // now if we want to increase the block of ptr we use realloc
+    // realloc() returns null if it fails to modify the block
+    // if null is assigned to ptr then there will be no ptr to block and it will be memory leak
+    // hence use temp ptr
+
+    int *temp = (int *)realloc(ptr, sizeof(int) * 4);
+    ptr = temp;
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", *(ptr + i));      // prints 4 0's
+    }
+
+    return 0;
+}
