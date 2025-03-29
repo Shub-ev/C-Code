d55e9a2faa26ccec18aa9dcb4b87dd58113ffac5
Shub-ev <shubhamg200204@gmail.com>
2025-03-29 20:38:52 +0530

    a

---
+++ DSA/01_recursion/Examples/02_factorial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int facto_tail(int n)
+{
+    if(n == 0) return 1;
+
+    return n * facto_tail(n-1); // tail
+}
+
+int facto_head(int n)
+{
+    if(n == 0) return 1;
+
+    return facto_head(n-1) * n;  // head
+}
+
+int main()
+{
+    int n;
+    printf("Number : ");
+    scanf("%d", &n);
+
+    printf("%d\n", facto_tail(n));
+    printf("%d\n", facto_head(n));
+
+    return 0;
+}
