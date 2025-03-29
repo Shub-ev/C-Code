f424274e64fdf9225b7bf8b660cec198a27938b8
Shub-ev <shubhamg200204@gmail.com>
2025-03-29 20:58:49 +0530

    a

--- DSA/01_recursion/Examples/03_power_of.c
+++ DSA/01_recursion/Examples/03_power_of.c
@@ -1,9 +1,29 @@
 #include<stdio.h>
 
+int power_tail(int b, int p)
+{
+    if(p == 0) return 1;
+    
+    return b * power_tail(b, p-1);
+}
 
+int power_head(int b, int p)
+{
+    if(p == 0) return 1;
+    
+    return power_tail(b, p-1) * b;
+}
 
 int main()
 {
+    int b, p;
+    printf("Base : ");
+    scanf("%d", &b);
+    printf("Power : ");
+    scanf("%d", &p);
+
+    printf("%d\n", power_tail(b,p));
+    printf("%d\n", power_head(b,p));
 
     return 0;
 }
