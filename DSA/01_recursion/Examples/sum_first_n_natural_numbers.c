2dc00d4964a1896f072b0ab2d05cbeaa81df1d5e
Shub-ev <shubhamg200204@gmail.com>
2025-03-29 20:02:28 +0530

    a

---
+++ DSA/01_recursion/Examples/sum_first_n_natural_numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int sum_n_natural_tail(int n){
+    if(n > 0){
+        return n + sum_n_natural_tail(n-1);  // tail recursion
+    }
+
+    return 0;
+}
+
+int sum_n_natural_head(int n){
+    if(n > 0){
+        return sum_n_natural_head(n-1) + n;  // head recursion
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int n;
+    printf("Enter Number : ");
+    scanf("%d", &n);
+
+    printf("%d\n", sum_n_natural_tail(n));
+    printf("%d\n", sum_n_natural_head(n));
+
+    return 0;
+}
--- DSA/dynamic_arr.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    int n;
-    printf("Enter the size of new array : ");
-    scanf("%d", &n);
-    int* arr = (int*) calloc(n, sizeof(int));
-}
