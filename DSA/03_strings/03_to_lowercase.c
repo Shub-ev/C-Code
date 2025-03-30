a28d9a219199d51b348ac961bbdf3ed7b54774db
Shub-ev <shubhamg200204@gmail.com>
2025-03-30 10:58:50 +0530

    a

---
+++ DSA/03_strings/03_to_lowercase.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch[] = "WELCOME";
+    printf("%s\n", ch);
+
+    for(int i = 0; i < sizeof(ch); i++){
+        ch[i] = (ch[i] - 'A') + 'a';
+    }
+
+    printf("%s\n", ch);
+    return 0;
+}
