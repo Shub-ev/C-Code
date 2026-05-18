c1ed820296f82f2e4e5cac3b83d90c072dec3602
shub-ev <shubhamg200204@gmail.com>
2026-05-18 13:11:11 +0530

    update remove option

--- DSA/02_array/array_ADT/main.c
+++ DSA/02_array/array_ADT/main.c
@@ -10,30 +10,35 @@ int main()
         printf("1. Display Array\n");
         printf("2. Append\n");
         printf("3. Insert\n");
-        // printf("4. Display Array\n");
+        printf("4. Remove\n");
         // printf("5. Display Array\n");
         
         printf("Enter Choince : ");
         scanf("%d", &n);
 
-        if(n == 1)
-            display(arr);
-        else if(n == 2)
-        {
-            int data;
-            printf("Enter data to Append : ");
-            scanf("%d", &data);
-            append(arr, data);
-        }
-        else if(n == 3)
-        {
-            int data, index;
-            printf("Enter data to Insert : ");
-            scanf("%d", &data);
-            printf("Enter Position : ");
-            scanf("%d", &index);
+        int data, index;
 
-            insert(arr, data, index);
+        switch(n) {
+            case 1:
+                display(arr);
+                break;
+            case 2:
+                printf("Enter data to Append : ");
+                scanf("%d", &data);
+                append(arr, data);
+                break;
+            case 3:
+                printf("Enter data to Insert : ");
+                scanf("%d", &data);
+                printf("Enter Position : ");
+                scanf("%d", &index);
+                insert(arr, data, index);
+                break;
+            case 4:
+                printf("Enter index: ");
+                scanf("%d", &index);
+                delete(arr, index);
+                break;
         }
     }while(n != 98);
     return 0;
